6a600554aad9423f74f6ab350ee44ca78ac8e02c
tr1pm <tripmulford@gmail.com>
2024-01-03 23:48:10 -0500

    added edited hourlyForecaster to match forecaster

--- downloadFunctions.py
+++ downloadFunctions.py
@@ -7,7 +7,6 @@ def forecaster(coordinates: list) -> list:
     lat = coordinates[0]
     lon = coordinates[1]
   
-
     responseObject = requests.get(f"https://api.weather.gov/points/{lat},{lon}")
 
     link1 = responseObject.json()['properties']["forecast"]
@@ -15,6 +14,21 @@ def forecaster(coordinates: list) -> list:
     forecastData = requests.get(link1).json()['properties']['periods']
     
     return forecastData
+
+
+def hourlyforecaster(coordinates: list) -> list:
+
+    lat = coordinates[0]
+    lon = coordinates[1]
+ 
+    responseObject = requests.get(f"https://api.weather.gov/points/{lat},{lon}")
+
+    link2 = responseObject.json()['properties']["forecastHourly"]
+
+    hourlyforecastData = requests.get(link2).json()['properties']['periods']
+
+    return hourlyforecastData
+
 
 #Returns a specified type of weather data over a specified number of 12 hour periods, maximum 14
 def forecasterr(forecastData: list, periods: int, weatherdataType: str) -> list:
@@ -28,5 +42,4 @@ def forecasterr(forecastData: list, periods: int, weatherdataType: str) -> list:
         generalForecasts.append(detailedForecast)
         index += 1
         
-
     return generalForecasts
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import downloadFunctions
 print(downloadFunctions.forecaster([38.99,-76.49]))
 
 dog = downloadFunctions.forecaster([38.99,-76.49])
+cat = downloadFunctions.hourlyforecaster([38.99,-76.49])
 
 
 print(downloadFunctions.forecasterr(dog, 2, 'detailedForecast'))
