e6998f1dcf0266b0ef985a9f538420d3830879ed
tr1pm <tripmulford@gmail.com>
2024-01-08 22:59:38 -0500

    added unfinished arguments file

---
+++ args.py
@@ -0,0 +1,58 @@
+
+coordinates = []
+
+
+#hourlyForecast data tyoes
+"number": 1,
+                "name": "",
+                "startTime": "2024-01-08T15:00:00-05:00",
+                "endTime": "2024-01-08T16:00:00-05:00",
+                "isDaytime": true,
+                "temperature": 45,
+                "temperatureUnit": "F",
+                "temperatureTrend": null,
+                "probabilityOfPrecipitation": {
+                    "unitCode": "wmoUnit:percent",
+                    "value": 0
+                },
+                "dewpoint": {
+                    "unitCode": "wmoUnit:degC",
+                    "value": -0.5555555555555556
+                },
+                "relativeHumidity": {
+                    "unitCode": "wmoUnit:percent",
+                    "value": 58
+                },
+                "windSpeed": "5 mph",
+                "windDirection": "E",
+                "icon": "https://api.weather.gov/icons/land/day/sct,0?size=small",
+                "shortForecast": "Mostly Sunny",
+                "detailedForecast": ""
+
+
+#forecastData types
+
+"name"
+"startTime"
+"endTime"
+"isDaytime"
+"temperature"
+"temperatureUnit"
+"temperatureTrend"
+"probabilityOfPrecipitation"{
+    "unitCode": "wmoUnit:percent",
+    "value": null}
+
+"dewpoint": {
+    "unitCode": "wmoUnit:degC",
+    "value": -0.5555555555555556
+}
+"relativeHumidity": {
+    "unitCode": "wmoUnit:percent",
+    "value": 58
+}
+"windSpeed"
+"windDirection"
+"icon"
+"shortForecast"
+"detailedForecast"
