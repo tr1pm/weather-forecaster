dfea68f2c42d4d90e048f9cafc4f9314aec93006
tr1pm <tripmulford@gmail.com>
2024-01-16 20:07:48 -0500

    added some text to main

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ print("Before we start, please enter a set of coordinates: ")
 
 float(input("x coordinates: "))
 float(input("y coordinates: "))
+
+print("Next, would you like forecasts over a 1 hour period or a 12 hour period?")
+print("Hour long periods, enter 0")
+print("12 hour long periods, enter 1")
