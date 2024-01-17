4c255ca1cef4547d7bcff58d999ad7bc824c7f17
tr1pm <tripmulford@gmail.com>
2024-01-16 21:12:31 -0500

    turned arguments into a list, added some text to main

--- args.py
+++ args.py
@@ -2,31 +2,30 @@
 #Gotta have a set of cooridnates
 coordinates = []
 
-
-
-b1 = "forecast"
-b2 = "forecastHourly"
+forecastTypes = [
+    "forecast",
+    "forecastHourly"
+]
 
 
 #All usable arguments for sortData()
-#They are arbitrarily named and should be renamed/organized
-#All return individual values except for a13-a15 which return dicts
-a1 = "name"
-a2 = "startTime"
-a3 = "endTime"
-a4 = "isDaytime"
-a5 = "temperature"
-a6 = "temperatureUnit"
-a7 = "temperatureTrend"
-a8 = "windSpeed"
-a9 = "windDirection"
-a10 = "icon"
-a11 = "shortForecast"
-a12 = "detailedForecast"
-
-a13 = "probabilityOfPrecipitation"
-a14 = "dewpoint"
-a15 = "relativeHumidity"
+dataTypes = [
+    "name",
+    "startTime",
+    "endTime",
+    "isDaytime",
+    "temperature",
+    "temperatureUnit",
+    "temperatureTrend",
+    "windSpeed",
+    "windDirection",
+    "icon",
+    "shortForecast",
+    "detailedForecast"
+    "probabilityOfPrecipitation",
+    "dewpoint",
+    "relativeHumidity",
+]
 
 #These are for getRain(), getHumidity(), and getDewpoint()
 c3 = "unitCode"
--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+import args
+import downloadFunctions
+
+
 print("Welcome to weather forecaster.")
+print()
 
 print("Before we start, please enter a set of coordinates: ")
 
 float(input("x coordinates: "))
 float(input("y coordinates: "))
+print()
 
 print("Next, would you like forecasts over a 1 hour period or a 12 hour period?")
 print("Hour long periods, enter 0")
 print("12 hour long periods, enter 1")
+
+
+int(input("enter here: "))
+print()
+
+
+print("here is all of the avaliable data.")
+i = 0
+while i < len(args.dataTypes):
+    print(f"{i} : {args.dataTypes[i]}")
+    i += 1
+print()
+print("Make as many selections as you want by entering a number and hitting enter. A blank entry indicates you are done.")
