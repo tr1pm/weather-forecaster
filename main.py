f469f833eacd11fbf7b129d7b8c4cf693971605b
tr1pm <tripmulford@gmail.com>
2024-01-18 18:58:11 -0500

    saved coordinates as a list. Changed code positioning

--- main.py
+++ main.py
@@ -1,48 +1,49 @@
 import args
 import downloadFunctions
 
+def main():
+    print("Welcome to weather forecaster.")
 
-print("Welcome to weather forecaster.")
-print()
+    print()
 
-print("Before we start, please enter a set of coordinates: ")
+    print("Before we start, please enter a set of coordinates: ")
+    xCoordinate = float(input("x coordinates: "))
+    yCoordinate = float(input("y coordinates: "))
+    coordinates = [xCoordinate, yCoordinate]
 
-float(input("x coordinates: "))
-float(input("y coordinates: "))
-print()
+    print()
 
-print("Next, would you like forecasts over a 1 hour period or a 12 hour period?")
-print("Hour long periods, enter 0")
-print("12 hour long periods, enter 1")
+    print("Next, would you like forecasts over a 1 hour period or a 12 hour period?")
+    print("for 1 hour long periods, enter 0")
+    print("for 12 hour long periods, enter 1")
+    ForecastTypeChoice = int(input("enter here: "))
 
+    print()
 
-int(input("enter here: "))
-print()
+    print("here is all of the avaliable data.")
+    i = 0
+    while i < len(args.dataTypes):
+        print(f"{i} : {args.dataTypes[i]}")
+        i += 1
+    print()
+    print("Make as many selections as you want by entering a number and hitting enter. A blank entry indicates you are done.")
 
+    #Working loop for entering the different types of desired forecast data. Needs work
+    RequestedDataTypes = []
+    isloopActive = True
+    while isloopActive == True:
+        x = input("Enter a number = ")
 
-print("here is all of the avaliable data.")
-i = 0
-while i < len(args.dataTypes):
-    print(f"{i} : {args.dataTypes[i]}")
-    i += 1
-print()
-print("Make as many selections as you want by entering a number and hitting enter. A blank entry indicates you are done.")
-
-#Working loop for entering the different types of desired forecast data. Needs work
-DataTypeList = []
-isloopActive = True
-while isloopActive == True:
-    x = input("Enter a number = ")
-
-    if x == "":
-        break
-    
-    if x.isdigit():
-        x = int(x)
+        if x == "":
+            break
         
-        if 0 <= x <= 13:
-            DataTypeList.append(x)
+        if x.isdigit():
+            x = int(x)
+            
+            if 0 <= x <= 13:
+                RequestedDataTypes.append(x)
+            else:
+                print("Please make a valid entry")
         else:
             print("Please make a valid entry")
-    else:
-        print("Please make a valid entry")
+
--- test.py
+++ test.py
@@ -1,31 +1,6 @@
 import downloadFunctions
+import main
 
 
-dog = downloadFunctions.downloadData([38.99,-76.49], "forecast")
-weatherdata2 = downloadFunctions.sortData(dog, 2, 'detailedForecast')
-print(weatherdata2)
-timess = downloadFunctions.sortData(dog, 2, "startTime")
-moose = downloadFunctions.writeTime(timess, weatherdata2)
-
-cat = downloadFunctions.downloadData([38.99,-76.49], "forecastHourly")
-weatherdata1 = downloadFunctions.sortData(cat, 12, 'probabilityOfPrecipitation')
-times = downloadFunctions.sortData(cat, 12, 'startTime') 
-stag = downloadFunctions.writeTime(times, weatherdata1, "degrees F")
-
-weatherdata3 = downloadFunctions.sortData(cat, 12, 'dewpoint')
-weatherdata4 = downloadFunctions.sortData(cat, 12, 'relativeHumidity')
-
-elk = downloadFunctions.getRain(weatherdata1, 12)
-clam = downloadFunctions.getDewpoint(weatherdata3, 12)
-toucan = downloadFunctions.getHumidity(weatherdata4, 12)
-
-print(1)
-print(2)
-
-
-forecastData = downloadFunctions.downloadData([38.99,-76.49], "forecast")
-detailedForecasts = downloadFunctions.sortData(forecastData, 2, "detailedForecast")
-
-
-HourlyForecastData = downloadFunctions.downloadData([38.99,-76.49], "forecastHourly")
-
+#Testing main
+main.main()
